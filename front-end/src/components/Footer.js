89f1df431c9677a64cedd688fda2685b1c4d5650
ImanSohail29 <imanmirza2197@gmail.com>
2024-01-12 01:08:57 +0500

    Footer component

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,12 +1,25 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faLocationDot, faMapLocationDot } from '@fortawesome/free-solid-svg-icons'
 
-const Footer=()=>{
+
+const Footer = () => {
     return (
         <footer>
             <Container fluid>
                 <Row>
                     <Col className="bg-dark text-white text-center py-5  bg-opacity-50">
-                        Copyright &copy; Shop Best Burgers Online
+                        Copyright &copy; Shop Best Burgers Online{" "}
+                        <a href="https://www.facebook.com/Burgercottagedha/" className="fa fa-facebook text-primary fs-3">
+                            <FontAwesomeIcon icon={faFacebook} ></FontAwesomeIcon>
+                        </a>{" "}
+                        <a href="https://www.instagram.com/burgercottage97?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="fa fa-instagram text-danger  fs-3">
+                            <FontAwesomeIcon icon={faInstagram} ></FontAwesomeIcon>
+                        </a>{" "}
+                        <a href="https://www.instagram.com/burgercottage97?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="fa fa-location text-success  fs-3">
+                            <FontAwesomeIcon icon={faMapLocationDot} ></FontAwesomeIcon>
+                        </a>
                     </Col>
                 </Row>
             </Container>
--- front-end/src/pages/components/CustomerHomePageComponent.js
+++ front-end/src/pages/components/CustomerHomePageComponent.js
@@ -12,7 +12,10 @@ const CustomerHomePageComponent = () => {
                     <Row className="justify-content-center">
                         <Col md={6} xs={8} lg={4} sm={6} >
                             <LinkContainer className="btn btn-warning border border-start opacity-75 w-100" to="/fooditem-list" >
-                                <Button className="p-4" variant="primary" size="lg"><h3>Place Your Order</h3><FontAwesomeIcon icon={faBurger} />{" "}<FontAwesomeIcon icon={faPizzaSlice} />{" "}<FontAwesomeIcon icon={faDrumstickBite} />{" "}<FontAwesomeIcon icon={faMartiniGlassCitrus} /></Button>
+                                <Button className="p-4" variant="primary" size="lg"><h3>Place Your Order</h3>
+                                
+                                <FontAwesomeIcon icon={faBurger} />{" "}<FontAwesomeIcon icon={faPizzaSlice} />{" "}<FontAwesomeIcon icon={faDrumstickBite} />{" "}<FontAwesomeIcon icon={faMartiniGlassCitrus} />
+                                </Button>
                             </LinkContainer>
                         </Col>
 
