5dfae2e2ec99e4e73e46473f6ddc5f1a398632db
ImanSohail29 <imanmirza2197@gmail.com>
2024-01-12 21:42:49 +0500

    Footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,7 +1,7 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faLocationDot, faMapLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot, faMapLocationDot,faPhone } from '@fortawesome/free-solid-svg-icons'
 
 
 const Footer = () => {
@@ -9,17 +9,31 @@ const Footer = () => {
         <footer>
             <Container fluid>
                 <Row>
-                    <Col className="bg-dark text-white text-center py-5  bg-opacity-50">
+                    <Col className="bg-dark text-white text-center text-muted py-4  bg-opacity-50">
+                        <small>
                         Copyright &copy; Shop Best Burgers Online{" "}
-                        <a href="https://www.facebook.com/Burgercottagedha/" className="fa fa-facebook text-primary  link-underline link-underline-opacity-0">
-                            <FontAwesomeIcon className="fs-3" icon={faFacebook} ></FontAwesomeIcon>Burger Cottage
-                        </a>{" "}
-                        <a href="https://www.instagram.com/burgercottage97?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="fa fa-instagram text-danger link-underline link-underline-opacity-0">
-                            <FontAwesomeIcon className="fs-3" icon={faInstagram} ></FontAwesomeIcon>burgercottage97
-                        </a>{" "}
-                        <a href="https://maps.app.goo.gl/GnKoHdgP7qgZXCJF9?g_st=ic" className="fa fa-location text-success">
-                            <FontAwesomeIcon className="fs-3" icon={faMapLocationDot} ></FontAwesomeIcon>
+                        <div>
+                        <a className="fa fa-location text-secondary link-underline link-underline-opacity-0">
+                            <FontAwesomeIcon className="fs-5" icon={faPhone} ></FontAwesomeIcon>{" "}03453007757
                         </a>
+                        </div>
+                        <div>
+                        <a href="https://www.facebook.com/Burgercottagedha/" className="fa fa-facebook text-primary  link-underline link-underline-opacity-0">
+                            <FontAwesomeIcon className="fs-5" icon={faFacebook} ></FontAwesomeIcon>{" "}Burger Cottage
+                        </a>
+                        </div>
+                        <div>
+                        <a href="https://www.instagram.com/burgercottage97?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="fa fa-instagram text-danger link-underline link-underline-opacity-0">
+                            <FontAwesomeIcon className="fs-5" icon={faInstagram} ></FontAwesomeIcon>{" "}burgercottage97
+                        </a>
+                        </div>
+                        <div>
+                        <a href="https://maps.app.goo.gl/GnKoHdgP7qgZXCJF9?g_st=ic" className="fa fa-location text-success link-underline link-underline-opacity-0">
+                            <FontAwesomeIcon className="fs-5" icon={faMapLocationDot} ></FontAwesomeIcon>{" "}89, Block C2 Block C 2 Nespak Housing Scheme, Lahore, Punjab
+                        </a>
+                        </div>
+                       
+                        </small>
                     </Col>
                 </Row>
             </Container>
