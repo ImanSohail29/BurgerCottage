2cf6bb5d83554bcf0cf6b3c8741ade3b2a217ba4
ImanSohail29 <imanmirza2197@gmail.com>
2024-01-16 18:45:23 +0500

    vendor component

--- back-end/models/VendorModel.js
+++ back-end/models/VendorModel.js
@@ -4,9 +4,9 @@ name:{type:String,required:true},
 phoneNumber:{type:String,required:true},
 email:{type:String},
 idcardNumber:{type:Number},
-amountRemaining:{type:Number},//subtract totalAmount-totalAmountPaid
-totalAmountPaid:{type:Number},//jub bhi pay ho isme add ho
-totalAmount:{type:Number}//jub bhi create ho isme add ho
+amountRemaining:{type:Number,default:0},//subtract totalAmount-totalAmountPaid
+totalAmountPaid:{type:Number,default:0},//jub bhi pay ho isme add ho
+totalAmount:{type:Number,default:0}//jub bhi create ho isme add ho
 })
 const Vendor = mongoose.model("Vendor", vendorSchema)
 module.exports = Vendor
